a6e474798ccc67ae59f299f61bf2dbcbb8595c1a
Enrique <enrique@test.com>
2020-05-21 22:22:31 +0100

    init

---
+++ RecaptchaV3/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RecaptchaV3.Models;
+
+namespace RecaptchaV3.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        private readonly IHttpClientFactory _clientFactory;
+        private static string googleSecretKey = "6LfsZ-IUAAAAAGygdpwQbp_riN6on7Lmjla6hKN0";
+        private static string googleRecaptchaVerifyApi = "https://www.google.com/recaptcha/api/siteverify";
+        public HomeController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(UserModel model)
+        {
+            var validUser = new UserModel() { Name = "huseyin", Password = "passwd" };
+            if (ModelState.IsValid)
+            {
+                if (validUser.Name == model.Name && validUser.Password == model.Password)
+                    return RedirectToAction("Welcome", "Home");
+                else
+                    return View();
+            }
+            return View();
+        }
+
+        public IActionResult Welcome()
+        {
+            ViewData["Message"] = "You are online now!";
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> TokenVerify(string token)
+        {
+            var verified = true;
+            TokenResponseModel tokenResponse = new TokenResponseModel() { Success = false };
+
+            using (var client = _clientFactory.CreateClient())
+            {
+                var response = await client.GetStringAsync($"{googleRecaptchaVerifyApi}?secret={googleSecretKey}&response={token}");
+                tokenResponse = JsonConvert.DeserializeObject<TokenResponseModel>(response);
+            }
+            // Recaptcha V3 Verify Api send score 0-1. If score is low such as less than 0.5, you can think that it is a bot and return false.     
+            // If token is not success then return false
+            if (!tokenResponse.Success || tokenResponse.Score < (decimal)0.5)
+                verified = false;
+            return Json(verified);
+        }
+    }
+}
